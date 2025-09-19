2060a021301b12378aea46904dfaf4ff2d8777f3
MUhammad Imran Butt <imranxfocusflow@gmail.com>
2025-09-19 11:08:58 +0500

    Added building class

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, WOrld")
+#this is the base Building class
+class Building:
+    def __init__(self,name,type,power = 0):
+        self.name = name
+        self.type = type
+        self.power = power
+    def supply_power(self,amount):
+        self.power += amount
+    def consume_energy(self,amount):
+        self.power -= amount
+    def __str__(self):
+        return f"------ Info ------\nName: {self.name}\nBuilding Type:{self.type}\Current Power:{self.power}"
+    def __eq__(self, value):
+        return self.type == value.type
+    
